38311c6ceb4653b4a167577056eb4d7900d0393c
Rustam Babaev <uktamych@yandex.ru>
2021-12-11 15:11:59 +0200

    fix: form placeholder fixed and was added err for search-form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function SearchForm() {
+  const [movie, setMovie] = useState("");
+  const [errmovie, setErrMovie] = useState("");
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
+  const handleChange = (evt) => {
+    const { value } = evt.target;
+    setMovie(value);
+    setErrMovie(evt.target.validationMessage);
+  };
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit} noValidate>
       <div action="" className="search-form__container">
-        <input type="text" className="search-form__input" name="Movie" defaultValue="Фильм" />
+        <input
+          type="text"
+          name="movie"
+          id="movie-input"
+          placeholder="Фильм"
+          minLength="2"
+          maxLength="50"
+          required
+          className="search-form__input"
+          onChange={handleChange}
+        />
         <input type="submit" value="Поиск" className="search-form__submit" />
       </div>
+      <span className="search-form__input-error">{errmovie}</span>
       <div class="search-form__filter-container">
         <div class="search-form__checkbox-container">
           <input type="checkbox" id="filterShortMovie" name="filterShortMovie" className="search-form__checkbox" />
